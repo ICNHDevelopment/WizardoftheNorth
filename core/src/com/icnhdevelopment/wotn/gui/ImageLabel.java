163cceabf49dac68ead2cfaa2ed5050fa733c469
Kyle <kyletherosenberg@icloud.com>
2015-09-10 16:46:01 -0400

    Nightly Build September 10, 2015 A

--- core/src/com/icnhdevelopment/wotn/gui/ImageLabel.java
+++ core/src/com/icnhdevelopment/wotn/gui/ImageLabel.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 /**
  * Created by kyle on 6/5/15.
