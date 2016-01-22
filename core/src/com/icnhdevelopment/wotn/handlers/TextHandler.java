fe8fa800a0bd4b5d85d88bd7c4bc7046414f2195
Xopiad <Xopiad@users.noreply.github.com>
2016-01-22 07:52:31 -0500

    Update TextHandler.java

--- core/src/com/icnhdevelopment/wotn/handlers/TextHandler.java
+++ core/src/com/icnhdevelopment/wotn/handlers/TextHandler.java
@@ -13,7 +13,7 @@ public class TextHandler {
 	final int maxCharactersPerLine = 0; //Change This Later
 	final int maxNumOfLines = 0; //Change This Later
  	
- 	public void scrollText(String text){
+ 	public ArrayList<String> textToLines(String text){
  		ArrayList<String> t = new ArrayList(Arrays.asList(text.split(" "))); //Added temporary parameter because errors
  		ArrayList<String> lines = new ArrayList<>();
  		String temp="";
@@ -22,10 +22,21 @@ public class TextHandler {
  		//Might change this into a for-each loop later
  		for (int i = 0; i < t.size(); i++){
  			if (t.get(i).length() + (count + 1) <= maxCharactersPerLine){// There will always be a space at the end, I don't want to make a seperate case.
- 				temp.concat(t.get(i) + " ");
- 				count += t.get(i).length() + 1;
+ 				temp.concat(t.get(i) + " "); //Adds text to string if total character length doesn't exceed max
+ 				count += t.get(i).length() + 1; //Updates characters count
  			}
  			else if (t.get(i).length() + (count + 1) <= maxCharactersPerLine){
+ 				count = 0;
+ 				lines.add(temp); //Sets concated string as line and starts concating a new line
+ 				temp = "";
+ 				
+ 				temp.concat(t.get(i) + " ");
+ 				count += count += t.get(i).length() + 1;
+ 			}
+ 			else if(lines.size() > maxNumOfLines){
+ 				//textBox.setText(1, "") where textBox.setText(int lineNumber, string text);
+ 				//or something like textBox.clear();
+ 				
  				count = 0;
  				lines.add(temp);
  				temp = "";
@@ -33,8 +44,13 @@ public class TextHandler {
  				temp.concat(t.get(i) + " ");
  				count += count += t.get(i).length() + 1;
  			}
- 			else if(lines.size() > maxNumOfLines){
- 				//Something about clearing textbox and adding new text
+ 		}
+ 	}
+ 	
+ 	public void scrollText(ArrayList<String> t, int scrollSpeed){
+ 		for (int i = 0; i < t.size(); i++){
+ 			for (int j = 0; j < t.get(i).length(); j++){
+ 				//Something
  			}
  		}
  	}
