b501bdc59fde68bdb28542293b1bc27a9f07862f
Xopiad <Xopiad@users.noreply.github.com>
2016-05-13 09:55:46 -0400

    Update TextHandler.java

--- core/src/com/icnhdevelopment/wotn/handlers/TextHandler.java
+++ core/src/com/icnhdevelopment/wotn/handlers/TextHandler.java
@@ -13,8 +13,10 @@ public class TextHandler {
 	final int maxCharactersPerLine = 0; //Change This Later
 	final int maxNumOfLines = 0; //Change This Later
  	
- 	public ArrayList<String> textToLines(String text){
- 		ArrayList<String> t = new ArrayList(Arrays.asList(text.split(" "))); //Added temporary parameter because errors
+ 	public static ArrayList<String> textToLines(String text){
+ 		ArrayList<String> t = new ArrayList(Arrays.asList(text.split("\\s+"))); //Added temporary parameter because errors
+ 		//System.out.println(t.size());
+ 		//System.out.println(t.get(0));
  		ArrayList<String> lines = new ArrayList<>();
  		String temp="";
  		int count = 0;
@@ -22,10 +24,11 @@ public class TextHandler {
  		//Might change this into a for-each loop later
  		for (int i = 0; i < t.size(); i++){
  			if (t.get(i).length() + (count + 1) <= maxCharactersPerLine){// There will always be a space at the end, I don't want to make a seperate case.
- 				temp.concat(t.get(i) + " "); //Adds text to string if total character length doesn't exceed max
+ 				temp = temp.concat(t.get(i).concat(" ")); //Adds text to string if total character length doesn't exceed max
  				count += t.get(i).length() + 1; //Updates characters count
+ 				if (i == t.size() - 1) lines.add(temp);
  			}
- 			else if (t.get(i).length() + (count + 1) <= maxCharactersPerLine){
+ 			else if (t.get(i).length() + (count + 1) >= maxCharactersPerLine){
  				count = 0;
  				lines.add(temp); //Sets concated string as line and starts concating a new line
  				temp = "";
@@ -45,7 +48,7 @@ public class TextHandler {
  				count += count += t.get(i).length() + 1;
  			}
  		}
-		return null;
+		return lines;
  	}
  	
  	public void scrollText(ArrayList<String> t, int scrollSpeed){
