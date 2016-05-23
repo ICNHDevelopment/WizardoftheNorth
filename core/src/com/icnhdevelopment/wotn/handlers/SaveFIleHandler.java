99380ac8dfbde4c608239c73ef62d816af106e65
Xopiad <Xopiad@users.noreply.github.com>
2016-05-23 15:56:02 -0400

    Update SaveFIleHandler.java
    Test it out

--- core/src/com/icnhdevelopment/wotn/handlers/SaveFIleHandler.java
+++ core/src/com/icnhdevelopment/wotn/handlers/SaveFIleHandler.java
@@ -7,37 +7,37 @@ import com.icnhdevelopment.wotn.world.World;
  */
 public class SaveFIleHandler {
 
-    public static void serialize(World w, File file){
-	    try {
-            FileOutputStream fileOut = new FileOutputStream(file);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(w);
-            out.close();
-            fileOut.close();
-        }
-        catch(IOException i) {
-            i.printStackTrace();
-        }
+	public static void serialize(World w, File file){
+		try {
+            		FileOutputStream fileOut = new FileOutputStream(file);
+            		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            		out.writeObject(w);
+            		out.close();
+            		fileOut.close();
+        	}
+        	catch(IOException i) {
+            		i.printStackTrace();
+        	}
 	}
 	
 	public static World deserialize(File file){
-	    World w = null;
-        try {
-            FileInputStream fileIn = new FileInputStream(file);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            w = (World) in.readObject();
-            in.close();
-            fileIn.close();
-            return w;
-        }
-        catch(IOException i) {
-            i.printStackTrace();
-            return null;
-        }
-        catch(ClassNotFoundException c) {
-            System.out.println("Employee class not found");
-            c.printStackTrace();
-            return null;
-        }
+		World w = null;
+        	try {
+            		FileInputStream fileIn = new FileInputStream(file);
+            		ObjectInputStream in = new ObjectInputStream(fileIn);
+            		w = (World) in.readObject();
+            		in.close();
+            		fileIn.close();
+            		return w;
+        	}
+        	catch(IOException i) {
+            		i.printStackTrace();
+            		return null;
+        	}
+        	catch(ClassNotFoundException c) {
+            		System.out.println("World class not found");
+            		c.printStackTrace();
+            		return null;
+        	}
 	}
 }
